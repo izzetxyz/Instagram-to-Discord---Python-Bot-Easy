6ba61bf8a9ebef74873e477a8bdcf4b706667da1
Khalid Almutairi <78239333+KhalidM30@users.noreply.github.com>
2023-03-24 11:53:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ COLOUR = 863385 #Discord Embed Colour
 WEBHOOK = 'https://discord.com/api/webhooks/' #Your Discord Webhook URL
 SLEEPTIME = 60 # SleepTime between each check
 IGTOKEN = "" # Your Instagram Long-Lived Access Tokens https://developers.facebook.com/docs/instagram-basic-display-api/guides/long-lived-access-tokens?locale=en_US#get-a-long-lived-token
-IGUSER_ID = 17841401887260051 # Instagram User ID https://developers.facebook.com/docs/instagram-basic-display-api/getting-started#step-5--exchange-the-code-for-a-token
+IGUSER_ID = 1111 # Instagram User ID https://developers.facebook.com/docs/instagram-basic-display-api/getting-started#step-5--exchange-the-code-for-a-token
